db476ebb3b61630714cd0d2bb33f7c55e993d520
ludovc <ludovcasu@gmail.com>
2024-11-23 17:27:33 +0100

    wip

--- memset.c
+++ memset.c
@@ -1,22 +1,35 @@
 #include "tester_libft.h"
 
-void	memset_test(int c)
+void	memset_test(int c, int n)
 {
-	char	*s;
+	void	*mem;
+	void	*mem2;
 
-	s = strdup("ciao");
-	ft_memset(s, c, 2);
-	//printf("%s\n", s);
-	printbytes(s, 4);
-	free(s);
-	s = strdup("ciao");
-	memset(s, c, 2);
-	//printf("%s\n", s);
-	puts("");
-	printbytes(s, 4);
+	mem = malloc(15);
+	memset(mem, 'j', 15);
+	ft_memset(mem, c, n);
+	mem2 = malloc(15);
+	memset(mem2, 'j', 15);
+	memset(mem2, c, n);
+	if (memcmp(mem, mem2, 15) == 0)
+	{
+		printf("ok\n");
+	}
+	else
+	{
+		printf("ko\n");
+		printf("\n");
+		printbytes(mem, 15);
+		printf("\n");
+		printbytes(mem2, 15);
+	}
 }
 
 int		main()
 {
-	memset_test(0);
+	memset_test('c', 5);
+	memset_test('c', 14);
+	memset_test('\n', 6);
+	memset_test('\0', 1);
+	memset_test('c' + 256, 5);
 }
