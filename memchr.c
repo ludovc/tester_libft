fb1b9ada3a12cb2fe9b84401b092504c95132b6e
Ludovico Casu <lucasu@c1r6p10.42firenze.it>
2024-11-25 17:49:08 +0100

    wip

--- memchr.c
+++ memchr.c
@@ -4,12 +4,13 @@
 void *ft_memchr2(const void *s, int c, size_t n)
 {
         size_t i;
-        unsigned char t;        i = 0;
+        unsigned char t;
+		i = 0;
         t = (unsigned char)c;        
 
 		if (n == 0)
 			return (NULL);
-		while(((unsigned char *)s)[i] != '\0' && i < n - 1)
+		while(((unsigned char *)s)[i] != '\0' && i < n)
         {
                 if(((unsigned char *)s)[i] == t)
                 {
@@ -23,9 +24,22 @@ void *ft_memchr2(const void *s, int c, size_t n)
 }
 */
 
+void	*ft_memchr2()
+{
+	void	*mem;
+
+	mem = malloc(1);
+	mem = NULL;
+	return (&mem);
+	/*
+	(void)mem;
+	return (NULL);
+	*/
+}
+
 int		main()
 {
-	char	mem[] = {0, 1, 2, 3, 4, 5};
+	//char	mem[] = {0, 1, 2, 3, 4, 5};
 
-	printf("%p\n", ft_memchr(mem, 0, 0));
+	printf("%p\n", ft_memchr2());
 }
