ca6e566a56e4086bddbff218bea164f2c6189b69
ludovc <ludovcasu@gmail.com>
2025-04-29 18:33:33 +0200

    wip

--- atoi.c
+++ atoi.c
@@ -4,4 +4,10 @@ int	main()
 {
 	ft_printf("%d %d\n", atoi("a"), ft_atoi("a"));
 	ft_printf("%d %d\n", atoi("0"), ft_atoi("0"));
+	ft_printf("%d %d\n", atoi("2147483647"), ft_atoi("2147483647"));
+	ft_printf("%d %d\n", atoi("2147483648"), ft_atoi("2147483648"));
+	ft_printf("%d %d\n", atoi("21474836470"), ft_atoi("21474836470"));
+	ft_printf("%d %d\n", atoi("-2147483648"), ft_atoi("-2147483648"));
+	ft_printf("%d %d\n", atoi("-2147483649"), ft_atoi("-2147483649"));
+	ft_printf("%d %d\n", atoi("-21474836480"), ft_atoi("-21474836480"));
 }
